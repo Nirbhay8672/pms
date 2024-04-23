6b1b9e7fb5133bcfb84800d6ac1bfef6fca982c9
Nirbhay8672 <71960388+Nirbhay8672@users.noreply.github.com>
2024-04-23 14:37:04 +0530

    Update

--- resources/views/google2fa/index.blade.php
+++ resources/views/google2fa/index.blade.php
@@ -1,50 +1,3 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center align-items-center " style="height: 70vh;">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading font-weight-bold">Register</div>
-                <hr>
-                @if($errors->any())
-                <div class="col-md-12">
-                    <div class="alert alert-danger">
-                        <strong>{{$errors->first()}}</strong>
-                    </div>
-                </div>
-                @endif
-
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('2fa') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group">
-                            <p>Please enter the <strong>OTP</strong> generated on your Authenticator App. <br> Ensure
-                                you submit the current one because it refreshes every 30 seconds.</p>
-                            <label for="one_time_password" class="col-md-4 control-label">One Time Password</label>
-
-                            <div class="col-md-6">
-                                <input id="one_time_password" type="number" class="form-control"
-                                    name="one_time_password" required autofocus>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4 mt-3">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -60,12 +13,10 @@
     <link rel="shortcut icon" href="{{ asset('/images/favicon.png') }}" />
 
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/bootstrap5.min.css') }}" rel="stylesheet">
 
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
